3d3b6c2c3ac171f2655fa0ac9459d23bc078dfe7
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-02-25 13:06:14 +0200

    Information Expert for Module

---
+++ JavaScript/4-module/ring.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const alloc = (size) => ({
+  size,
+  buffer: Buffer.alloc(size),
+  offset: 0,
+});
+
+const write = (buf, data) => {
+  const { size, offset } = buf;
+  const { length } = data;
+  const available = size - offset;
+  const len = Math.min(available, size, length);
+  const rest = available - length;
+  buf.buffer.write(data, offset, len);
+  buf.offset += len;
+  if (buf.offset === size) buf.offset = 0;
+  if (rest < 0) write(buf, data.slice(rest));
+};
+
+module.exports = { alloc, write };
